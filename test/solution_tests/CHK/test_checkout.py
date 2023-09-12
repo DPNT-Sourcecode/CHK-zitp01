7168fbb3663ca048655b75de7e04c479deb3641f
sbx_user1051 <sbx_user1051@169.254.9.129>
2023-09-12 12:22:08 +0000

    Tue Sep 12 12:22:08 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,8 +1,18 @@
 from solutions.CHK import checkout_solution
 
 class TestCheckout():
+    def test_invalid_input(self):
+        assert checkout_solution.checkout("1?42") == -1
+        assert checkout_solution.checkout("9FNA") == -1
+
     def test_checkout(self):
         assert checkout_solution.checkout("ABC") == 100
+
+    def test_checkout_empty(self):
+        assert checkout_solution.checkout("") == 0
+
+    def test_checkout_with_duplicates(self):
+        assert checkout_solution.checkout("ABCCAD") == 185
 
     def test_checkout_with_offers(self):
         assert checkout_solution.checkout("AAABDBB") == 220
